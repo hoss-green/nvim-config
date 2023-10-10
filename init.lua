5cd5740253131d5147430461d16e9d4368512a24
Hoss <hoss.green@pm.me>
2023-10-10 10:39:00 +0200

    added in <Leader>a to go to definition

--- init.lua
+++ init.lua
@@ -20,22 +20,22 @@ dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
 
-local rt = require("rust-tools")
+local rt = require "rust-tools"
 
-rt.setup({
+rt.setup {
   server = {
     settings = {
-      ['rust-analyzer'] = {
+      ["rust-analyzer"] = {
         cargo = {
-          features = "all"
-        }
-      }
+          features = "all",
+        },
+      },
     },
     on_attach = function(_, bufnr)
       -- Hover Actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, {buffer = bufnr})
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
       -- Code action gouprs
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, {buffer = bufnr})
+      -- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
   tools = {
@@ -43,116 +43,117 @@ rt.setup({
     inlay_hints = {
       auto = true,
       only_current_line = false,
-      highlight = "Comment"
+      highlight = "Comment",
     },
     hover_actions = {
-      auto_focus = true
-    }
-  }
-})
+      auto_focus = true,
+    },
+  },
+}
 
 rt.inlay_hints.enable()
 
--- LSP Diagnostics Options Setup 
+-- LSP Diagnostics Options Setup
 local sign = function(opts)
   vim.fn.sign_define(opts.name, {
     texthl = opts.name,
     text = opts.text,
-    numhl = ''
+    numhl = "",
   })
 end
 
-sign({name = 'DiagnosticSignError', text = ''})
-sign({name = 'DiagnosticSignWarn', text = ''})
-sign({name = 'DiagnosticSignHint', text = ''})
-sign({name = 'DiagnosticSignInfo', text = ''})
+sign { name = "DiagnosticSignError", text = "" }
+sign { name = "DiagnosticSignWarn", text = "" }
+sign { name = "DiagnosticSignHint", text = "" }
+sign { name = "DiagnosticSignInfo", text = "" }
 
-vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = false,
-    float = {
-        border = 'rounded',
-        source = 'always',
-        header = '',
-        prefix = '',
-    },
-})
+vim.diagnostic.config {
+  virtual_text = false,
+  signs = true,
+  update_in_insert = true,
+  underline = true,
+  severity_sort = false,
+  float = {
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+}
 
-vim.cmd([[
+vim.cmd [[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-]])
+]]
 
 -- Completion Plugin Setup
-local cmp = require('cmp')
-cmp.setup({
+local cmp = require "cmp"
+cmp.setup {
   -- Enable LSP snippets
   snippet = {
     expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
+      vim.fn["vsnip#anonymous"](args.body)
     end,
   },
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
     -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
-    })
+    },
   },
   -- Installed sources:
   sources = {
-    { name = 'path' },                              -- file paths
-    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
-    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
-    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
-    { name = 'buffer', keyword_length = 2 },        -- source current buffer
-    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
-    { name = 'calc'},                               -- source for math calculation
+    { name = "path" }, -- file paths
+    { name = "nvim_lsp", keyword_length = 3 }, -- from language server
+    { name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
+    { name = "nvim_lua", keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = "buffer", keyword_length = 2 }, -- source current buffer
+    { name = "vsnip", keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
+    { name = "calc" }, -- source for math calculation
   },
   window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   formatting = {
-      fields = {'menu', 'abbr', 'kind'},
-      format = function(entry, item)
-          local menu_icon ={
-              nvim_lsp = 'λ',
-              vsnip = '⋗',
-              buffer = 'Ω',
-              path = '🖫',
-          }
-          item.menu = menu_icon[entry.source.name]
-          return item
-      end,
+    fields = { "menu", "abbr", "kind" },
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = "λ",
+        vsnip = "⋗",
+        buffer = "Ω",
+        path = "🖫",
+      }
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
   },
-})
+}
 
--- Treesitter Plugin Setup 
-require('nvim-treesitter.configs').setup {
+-- Treesitter Plugin Setup
+require("nvim-treesitter.configs").setup {
   ensure_installed = { "lua", "rust", "toml" },
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting=false,
+    additional_vim_regex_highlighting = false,
   },
-  ident = { enable = true }, 
+  ident = { enable = true },
   rainbow = {
     enable = true,
     extended_mode = true,
     max_file_lines = nil,
-  }
+  },
 }
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+vim.keymap.set("n", "<Leader>a", vim.lsp.buf.declaration)
